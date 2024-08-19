95b6ad860368695330a3f1facbabea7c5bfe6903
Luciano Vettoretti <luciano.vittoretti@gmail.com>
2024-08-19 14:03:09 -0300

    incluindo pasta do XAMPP para sincronização com o OneDrive

--- sync_projects_to_onedrive.sh
+++ sync_projects_to_onedrive.sh
@@ -15,6 +15,10 @@ echo "Sincronização de projetos iniciada..." >> "$LOG_FILE"
 echo "Criando pasta temporária, se não existir..." >> "$LOG_FILE"
 mkdir -p "$TEMP_DIR"
 
+# Criação da pasta temporária do XAMPP se não existir
+echo "Criando pasta temporária do XAMPP, se não existir..." >> "$LOG_FILE"
+mkdir -p "$TEMP_DIR/xampp"
+
 # Verificação de status do Git em todos os subdiretórios
 echo "Verificando status dos repositórios Git em $PROJECTS_DIR..." >> "$LOG_FILE"
 cd "$PROJECTS_DIR"
@@ -35,6 +39,10 @@ done
 # Sincroniza o diretório de projetos para um diretório temporário
 echo "Sincronizando o diretório de projetos para a pasta temporária..." >> "$LOG_FILE"
 rsync -a --delete "$PROJECTS_DIR/" "$TEMP_DIR/"
+
+# Sincroniza o diretório do XAMPP para um diretório temporário
+echo "Sincronizando o diretório do XAMPP para a pasta temporária..." >> "$LOG_FILE"
+rsync -a --delete "/mnt/c/xampp/" "$TEMP_DIR/xampp/"
 
 # Remove o último snapshot do OneDrive
 echo "Removendo o último snapshot do OneDrive..." >> "$LOG_FILE"
