e477699385b3c70cd2801a56e537a2c81223be4a
Luciano Leite <luciano.vittoretti@gmail.com>
2024-09-03 15:35:00 -0300

    alterando ordem de execução de alguns comandos

--- sync_projects_to_onedrive.sh
+++ sync_projects_to_onedrive.sh
@@ -61,10 +61,6 @@ rsync -a --delete "$PROJECTS_DIR/" "$TEMP_DIR/projects/Dev/"
 echo "Sincronizando o diretório do XAMPP para a subpasta temporária 'projects'..." >> "$LOG_FILE"
 rsync -a --delete "/mnt/c/xampp/" "$TEMP_DIR/projects/xampp/"
 
-# Remove o último snapshot do OneDrive
-echo "Removendo o último snapshot do OneDrive..." >> "$LOG_FILE"
-find "$ONEDRIVE_DIR" -type f -name "snapshot_*.tar.gz.gpg" -exec rm -f {} +
-
 # Cria um novo snapshot com a data e hora atuais e armazena na subpasta 'archive'
 SNAPSHOT_NAME="snapshot_$(date +'%Y%m%d_%H%M%S').tar.gz"
 echo "Criando um novo snapshot: $SNAPSHOT_NAME..." >> "$LOG_FILE"
@@ -74,8 +70,12 @@ tar -czf "$TEMP_DIR/archive/$SNAPSHOT_NAME" -C "$TEMP_DIR/projects" .
 echo "Criptografando o snapshot..." >> "$LOG_FILE"
 gpg --batch --yes --passphrase "$PASSWORD" --symmetric --cipher-algo AES256 -o "$TEMP_DIR/archive/$SNAPSHOT_NAME.gpg" "$TEMP_DIR/archive/$SNAPSHOT_NAME"
 
+# Remove o último snapshot do OneDrive
+echo "Removendo o último snapshot do OneDrive..." >> "$LOG_FILE"
+find "$ONEDRIVE_DIR" -type f -name "snapshot_*.tar.gz.gpg" -exec rm -f {} +
+
 # Move o arquivo criptografado para o OneDrive
-echo "Movendo o arquivo criptografado para o OneDrive..." >> "$LOG_FILE"
+echo "Movendo o novo snapshot criptografado para o OneDrive..." >> "$LOG_FILE"
 mv "$TEMP_DIR/archive/$SNAPSHOT_NAME.gpg" "$ONEDRIVE_DIR/"
 
 # Remove o arquivo compactado não criptografado
